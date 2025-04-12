8a271b392f2e0989459398902dc1edd4578eaff7
Star <snabajja@gmail.com>
2025-04-12 00:30:06 -0400

    Add linkedIn link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
     <footer>
       <a href="https://github.com/snabaj" target="_blank" rel="noopener noreferrer">GitHub</a>
-      <a href="#linkedin" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="www.linkedin.com/in/stella-nabajja-09052535b" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       <a href="mailto:snabajja@gmail.com">Email</a>
       <a href="tel:+8573127900">Phone</a>
     </footer>
