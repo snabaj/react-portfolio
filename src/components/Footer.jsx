74e9dcf5baa5bf1b9ba1e6b2f6906c3b9f215271
Star <snabajja@gmail.com>
2025-04-12 14:45:11 -0400

    Refactor footer component to use image icons for GitHub and LinkedIn links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import githubMark from "../assets/images/github-mark-white.png";
+import linkedIn from "../assets/images/LI-Logo.png";
 
 function Footer() {
     return (
     <footer>
-      <a href="https://github.com/snabaj" target="_blank" rel="noopener noreferrer">GitHub</a>
-      <a href="www.linkedin.com/in/stella-nabajja-09052535b" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      <a href="mailto:snabajja@gmail.com">Email</a>
-      <a href="tel:+8573127900">Phone</a>
+      <a href="https://github.com/snabaj" target="_blank" rel="noopener noreferrer"><img src={githubMark} id='github' alt="github repo" /></a>
+      <a href="https://www.linkedin.com/in/stella-nabajja-09052535b" target="_blank" rel="noopener noreferrer"><img src={linkedIn} id='linkedin' alt="linkedin profile" /></a>
+      <a href="mailto:snabajja@gmail.com">📧</a>
+      {/* <a href="tel:+8573127900">Phone</a> */}
     </footer>
     );
 };
