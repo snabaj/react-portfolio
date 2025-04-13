7897cc3f52697aa7176bc12c485df2e8df48af72
Star <snabajja@gmail.com>
2025-04-13 19:18:44 -0400

    Refactor Project component to add project description and deployedLink

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 
-    function Project({ title, imageSrc, repoLink }) {
-        const handleIconClick = () => {
-          window.open(repoLink, "_blank", "noopener, noreferrer");
-        };
+function Project({ title, description, imageSrc, deployedLink, repoLink }) {
+  // const handleIconClick = () => {
+  //   window.open(repoLink, "_blank", "noopener, noreferrer");
+  // };
 
-        return (
-            <div className="project">
-            <img src={imageSrc} alt={title} className="project-image" />
-            <div className="project-info">
-              <h3 className="project-title">{title}</h3>
-              <i
+  return (
+    <div className="project">
+      <img src={imageSrc} alt={title} className="project-image" />
+      <div className="project-info">
+        <h3 className="project-title">{title}</h3>
+        {/* <i
                 className="fab fa-github repo-icon"
                 onClick={handleIconClick}
                 title={`View ${title} on GitHub`}
                 style={{ cursor: "pointer" }}
-              ></i>
-            </div>
-          </div>
-        );
-      }
+              ></i> */}
+        <p>{description}</p>
+        <div className="project-buttons">
+          <a
+            href={deployedLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn"
+          >
+            View Project
+          </a>
+          <a
+            href={repoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn"
+          >
+            View Repo
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Project;
