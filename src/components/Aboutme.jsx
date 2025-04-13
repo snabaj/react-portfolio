53fa3d15dd2f427971ea8eabb3b79005cae7ed31
Star <snabajja@gmail.com>
2025-04-13 19:15:59 -0400

    Refactor Aboutme component to remove commented out code and update content

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import profileImage from "../assets/images/profile-image.png";
 import portfolioImage from "../assets/images/Portfolio_3.jpg";
 
 function AboutMe() {
@@ -19,7 +18,7 @@ I’m well-versed in GitHub for version control, and I’m learning the ropes of
 
 Beyond solo projects, I thrive in collaboration — I love working with other developers, sharing ideas, solving problems, and leveling up together. My portfolio showcases a range of projects — from solo builds to team collaborations developed during my coding bootcamp that taught me the power of working together to bring big ideas to life.
 
-<br/><br/>When I’m not coding, you’ll find me riding my bike (yep — I just learned recently as an adult 👏), reading literary fiction, exploring new places, or planning my next adventure.
+<br/><br/>When I’m not coding, you’ll find me riding my bike, reading literary fiction, exploring new places, or planning my next adventure.
 
 Let’s connect, build something great, and maybe share a laugh or two along the way. 🚀
       </p>
