62b68b171066b90f84ccdebeddf3d0cbee58171b
Star <snabajja@gmail.com>
2025-04-14 17:19:40 -0400

    Add formatting

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-  function Navigation({ currentSection, setCurrentSection }) {
-    return (
+function Navigation({ currentSection, setCurrentSection }) {
+  return (
     <nav>
-      {['Home', 'About Me', 'Contact', 'Resume'].map((section) => (
+      {["Home", "About Me", "Contact", "Resume"].map((section) => (
         <button
           key={section}
-          className={currentSection === section ? 'active' : ''}
+          className={currentSection === section ? "active" : ""}
           onClick={() => setCurrentSection(section)}
         >
           {section}
         </button>
       ))}
     </nav>
-    );
-  };
-  
-  export default Navigation;
+  );
+}
+
+export default Navigation;
