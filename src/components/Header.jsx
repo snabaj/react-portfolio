06aaaefc19a41bd33071ed0c7c9d3bb84f5123d5
Star <snabajja@gmail.com>
2025-04-14 17:19:14 -0400

    Add formatting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Navigation from './Navigation';
+import React from "react";
+import Navigation from "./Navigation";
 
 function Header({ currentSection, setCurrentSection }) {
-    return (
+  return (
     <header>
       <h1>Stella Nabajja</h1>
-      <Navigation currentSection={currentSection} setCurrentSection={setCurrentSection} />
+      <Navigation
+        currentSection={currentSection}
+        setCurrentSection={setCurrentSection}
+      />
     </header>
-    );
-};
-  
+  );
+}
+
 export default Header;
-  
