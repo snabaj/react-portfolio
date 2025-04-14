8eec1ceb3032e621478159370257940e17decf15
Star <snabajja@gmail.com>
2025-04-14 17:19:50 -0400

    Refactor Project component to remove commented out code and improve code readability

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,21 +1,11 @@
 import React from "react";
 
 function Project({ title, description, imageSrc, deployedLink, repoLink }) {
-  // const handleIconClick = () => {
-  //   window.open(repoLink, "_blank", "noopener, noreferrer");
-  // };
-
   return (
     <div className="project-card">
       <img src={imageSrc} alt={title} className="project-image" />
       <div className="project-info">
         <h3 className="project-title">{title}</h3>
-        {/* <i
-                className="fab fa-github repo-icon"
-                onClick={handleIconClick}
-                title={`View ${title} on GitHub`}
-                style={{ cursor: "pointer" }}
-              ></i> */}
         <p>{description}</p>
         <div className="project-buttons">
           <a
