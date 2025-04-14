3ce902e1e5acd067c6e13fca26c87d4beef153c3
Star <snabajja@gmail.com>
2025-04-14 17:19:01 -0400

    Refactor Footer component to improve code readability and formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,29 @@ import githubMark from "../assets/images/github-mark-white.png";
 import linkedIn from "../assets/images/LI-Logo.png";
 
 function Footer() {
-    return (
+  return (
     <footer>
-      <a href="https://github.com/snabaj" target="_blank" rel="noopener noreferrer"><img src={githubMark} id='github' alt="github repo" /></a>
-      <a href="https://www.linkedin.com/in/stella-nabajja-09052535b" target="_blank" rel="noopener noreferrer"><img src={linkedIn} id='linkedin' alt="linkedin profile" /></a>
+      <nav>
+        <a
+          href="https://github.com/snabaj"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={githubMark} id="github" alt="github repo" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/stella-nabajja-09052535b"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedIn} id="linkedin" alt="linkedin profile" />
+        </a>
+      </nav>
       {/* <a href="mailto:snabajja@gmail.com">📧</a> */}
-      {/* <a href="tel:+8573127900">Phone</a> */}
+      {/* <a href="tel:+85">Phone</a> */}
+      <p>&copy; 2025 Stella Nabajja. All rights reserved.</p>
     </footer>
-    );
-};
+  );
+}
 
 export default Footer;
