2556e28a379c1b7966274a117007818b786bb6d8
Star <snabajja@gmail.com>
2025-05-05 13:41:22 -0400

    Refactor header title and project grid layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 function Header({ currentSection, setCurrentSection }) {
   return (
     <header>
-      <h1>Stella Nabajja</h1>
+      <h1>Stella's Portfolio</h1>
       <Navigation
         currentSection={currentSection}
         setCurrentSection={setCurrentSection}
